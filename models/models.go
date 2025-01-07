8422522ab75d00f55f36382cb0fb97b38daa0374
Evgeniy Fedotov <egene@mbpm113.local>
2025-01-07 07:26:14 +0300

    initial project

--- models/models.go
+++ models/models.go
@@ -1,11 +1,11 @@
- // models/models.go
+// models/models.go
 package models
 
 import "gorm.io/gorm"
 
 type Task struct {
-    gorm.Model
-    ID          uint    `json:"id" gorm:"primaryKey"`
-    Description string  `json:"description" gorm:"text"`
-    Note        string  `json:"note" gorm:"text"`
+	gorm.Model
+	ID          uint   `json:"id" gorm:"primaryKey"`
+	Description string `json:"description" gorm:"text"`
+	Note        string `json:"note" gorm:"text"`
 }
